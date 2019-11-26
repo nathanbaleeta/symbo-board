e8405d4150cb4acffe433de9d3b4780495fe6c68
nathanbaleeta <nbaleeta@gmail.com>
2019-11-26 20:23:29 +0300

    Added middleware for CORS

--- symboboard/symboboard/settings.py
+++ symboboard/symboboard/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -59,7 +60,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'symboboard.urls'
 
