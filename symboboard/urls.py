b90336c5bb2b849efb558f3d4643623153c7dac8
nathanbaleeta <nbaleeta@gmail.com>
2019-11-28 12:39:13 +0300

    Added user endpoint to account app

--- symboboard/urls.py
+++ symboboard/urls.py
@@ -2,28 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
-# Serializers define the API representation.
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+from apps.account.api.user.user_endpoint import userRouter
 
 
 urlpatterns = [
@@ -31,7 +12,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 
-    url(r'^api/', include(router.urls)),
+    url(r'^api/', include(userRouter.urls)),
+
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
