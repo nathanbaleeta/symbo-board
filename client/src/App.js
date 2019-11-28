9d70bc752f2c83e604d0679236446cda31b11e69
nathanbaleeta <nbaleeta@gmail.com>
2019-11-28 14:06:48 +0300

    Added authentication to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+
+import UnauthedRoute from "./Components/UnauthedRoute";
+import AuthedRoute from "./Components/AuthedRoute";
+
+const App = () => (
+  <Router>
+    <Switch>
+      <UnauthedRoute path="/auth/login" component={Login} />
+      <AuthedRoute path="/" component={Home} />
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ client/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+
+class Home extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Nathan</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
